fce427d73ed5c6a2c24310d34cce1ccb7171bf52
Barpadzik <bartek.nowakx08@gmail.com>
2025-05-01 17:48:50 +0200

    Fixed a bug with effects not being removed in cases where after shutting down the server and re-entering the player teleports him to the spawn with the effects of the zone he was in before the server was stopped

--- src/main/java/pl/barpad/duckyzones/Main.java
+++ src/main/java/pl/barpad/duckyzones/Main.java
@@ -1,30 +1,47 @@
 package pl.barpad.duckyzones;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 import pl.barpad.duckyzones.listeners.ZoneListener;
 import pl.barpad.duckyzones.managers.ConfigManager;
 import pl.barpad.duckyzones.managers.MessagesManager;
 import pl.barpad.duckyzones.managers.ZoneManager;
+import pl.barpad.duckyzones.model.Zone;
 
 public class Main extends JavaPlugin {
+
+    private ZoneManager zoneManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         int serviceId = 25524;
         new MetricsLite(this, serviceId);
+
         ConfigManager configManager = new ConfigManager(this);
-        ZoneManager zoneManager = new ZoneManager(this);
+        this.zoneManager = new ZoneManager(this);
         MessagesManager messagesManager = new MessagesManager(this);
         ZoneListener zoneListener = new ZoneListener(this, zoneManager, messagesManager);
-        getServer().getPluginManager().registerEvents(new ZoneListener(this, zoneManager, messagesManager), this);
+
+        getServer().getPluginManager().registerEvents(zoneListener, this);
         new Reload(this, configManager, messagesManager, zoneManager, zoneListener);
         new UpdateChecker(this).checkForUpdates();
+
         getLogger().info("DuckyZones Enabled | Author: Barpad!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("DuckyZones Disabled | Author: Barpad");
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            for (Zone zone : zoneManager.getZones()) {
+                if (zone.isInside(player.getLocation())) {
+                    for (PotionEffectType type : zone.getZoneEffects().keySet()) {
+                        player.removePotionEffect(type);
+                    }
+                }
+            }
+        }
     }
 }
